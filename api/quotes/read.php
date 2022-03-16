0f6376b675421da52dc6c705e515b442c81bddaf
donnatiu <89421448+donnatiu@users.noreply.github.com>
2022-03-16 14:02:53 -0700

    Update READ operation

--- api/quotes/read.php
+++ api/quotes/read.php
@@ -34,7 +34,7 @@
   if($num > 0) {
     // Quote array
     $quote_arr = array();
-    $quote_arr['data'] = array();
+    //$quote_arr['data'] = array();
 
     while($row = $result->fetch(PDO::FETCH_ASSOC)) {
       extract($row);
@@ -47,7 +47,8 @@
       );
 
       // Push to "data"
-      array_push($quote_arr['data'], $quote_item);
+      array_push($quote_arr, $quote_item);
+      // array_push($quote_arr['data'], $quote_item);
     }
 
     // Turn to JSON & output
